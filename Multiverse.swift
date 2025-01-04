609fc7d932efea1a8f48ac708d1dcaecc4741516
L <60981745+pankourgiou@users.noreply.github.com>
2025-01-04 10:40:53 +0200

    Create Multiverse.swift

---
+++ Multiverse.swift
@@ -0,0 +1,2 @@
+//You will have to meet nebula and various phenomena but this is it!
+print("You will have to drive your mind to the absolute limits of universe and galaxies so that two people eventually come close.")
